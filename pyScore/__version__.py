37fa7712f9f24e1b4ddfff588e0f8bf70a37a06b
Michael Droettboom <mdboom@jhu.edu>
2004-05-07 12:17:19 +0000

    Minor documentation changes.

--- pyScore/__version__.py
+++ pyScore/__version__.py
@@ -1,4 +1,4 @@
-version = "0.3"
+version = "0.3.1"
 url = "http://dkc.jhu.edu/~mdboom/pyScore"
 author = "Michael Droettboom"
 created = "Created with pyScore %s by %s <%s>" % (version, author, url)
